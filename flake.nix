c7ce188bb3e7c8d63193ad6e5ffcc6bbaa36d333
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:17 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Static mock authentication website for recording login and credential update flows";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [ python3 ];
+      };
+    });
+}
