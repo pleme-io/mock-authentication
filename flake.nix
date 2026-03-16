f659eb44325232c2dd445b6cf64134e49a5cc63e
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:03 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
   description = "Static mock authentication website for recording login and credential update flows";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ python3 ];
-      };
-    });
+
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "shell";
+      description = "Static mock authentication website for recording login and credential update flows";
+    };
 }
